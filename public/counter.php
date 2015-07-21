596cc14c606cf762d0ae18239f1469940d8e404d
asteph <alissa.stephens1@gmail.com>
2015-07-21 12:00:46 -0500

    has two links that increase or decrease the counter value when clicked

---
+++ public/counter.php
@@ -0,0 +1,45 @@
+<?php
+
+// Require or include statements are allowed here. All other code goes in the pageController function.
+
+/**
+ * The pageController function handles all processing for this page.
+ * @return array An associative array of data to be used in rendering the html view.
+ */
+function pageController()
+{
+    var_dump($_GET);
+    if (empty($_GET)){
+        $counter = 0;
+    }else if($_GET['direction'] == 'up'){
+        $counter = (int)$_GET['counter'] + 1;
+    }else if ($_GET['direction'] == 'down'){
+        $counter = (int)$_GET['counter'] - 1;
+    }
+
+    // Initialize an empty data array.
+    $data = array();
+
+    // Add data to be used in the html view.
+    $data['value'] = $counter;
+
+    // Return the completed data array.
+    return $data;    
+}
+
+// Call the pageController function and extract all the returned array as local variables.
+extract(pageController());
+
+// Only use echo, conditionals, and loops anywhere within the HTML.
+?>
+<!doctype html>
+<html>
+    <head>
+        <title>Counter</title>
+    </head>
+    <body>
+        <h2>Current counter value: <?= $value?></h2>
+        <a href="?direction=up&counter=<?= $value ?>">UP</a>
+        <a href="?direction=down&counter=<?= $value ?>">DOWN</a>
+    </body>
+</html>

adbf68b03939e369ab46440ed51b3095941cef95
asteph <alissa.stephens1@gmail.com>
2015-07-22 10:25:58 -0500

    Post examples and exercises that execute an action if the correct information is posted

---
+++ public/login.php
@@ -0,0 +1,28 @@
+<?php
+var_dump($_POST);
+$message = null;
+if(!empty($_POST)){
+	if($_POST['username'] == 'guest' && $_POST['password'] == 'password'){
+		header('Location: authorized.php');
+	}else{
+		$message = 'Login Failed';
+	}
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <form method="POST">
+        <label>username</label>
+        <input type="text" name="username"><br>
+        <label>password</label>
+        <input type="password" name="password"><br>
+        <input type="submit">
+    </form>
+    <?= $message ?>
+</body>
+</html>

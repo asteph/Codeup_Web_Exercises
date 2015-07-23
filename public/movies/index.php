0df619006da3801ed485dec605c49b5b761a56ca
asteph <alissa.stephens1@gmail.com>
2015-07-23 10:41:07 -0500

    deletes movies past index zero, but doesn't delete first item in array TODO: look into using something other than isset()

--- public/movies/index.php
+++ public/movies/index.php
@@ -1,7 +1,15 @@
 <?php 
 	session_start();
 	$moviesArray = array();
+	//look at posted information
 	var_dump($_POST);
+	//if delete has been pressed, delete movie associated with that button	
+	if(isset($_POST['delete'])){
+		$position = (int)$_POST['delete'];
+		$_SESSION['movies'] = array_splice($_SESSION['movies'], $position , 1);
+		var_dump($_SESSION['movies']);
+		unset($_POST['delete']);
+	}
 	// check to see if there are any movies
 	if (!empty($_SESSION['movies'])) {
 	    $moviesArray = $_SESSION['movies'];
@@ -35,6 +43,9 @@
     	<li>
     	<a href="show.php?index=<?= $key ?>"><?= $movie['title']?></a>
     	</li>
+	    <form method = "POST">
+        	<input type="submit" name="delete" value="<?= $key ?>" />
+    	</form>
 	<? endforeach; ?>
 	</ul>
 </body>

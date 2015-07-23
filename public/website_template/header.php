a1009ee568aeee934e02dd405658e90384cfb2a3
asteph <alissa.stephens1@gmail.com>
2015-07-23 12:05:25 -0500

    website template made for practice combining multiple files using the request function

---
+++ public/website_template/header.php
@@ -0,0 +1,8 @@
+<!-- bootstrap page header -->
+<div class="container">
+  <div class="page-header">
+    <h1>Example Page Header</h1>      
+  </div>
+  <p>This is some text.</p>      
+  <p>This is another text.</p>      
+</div>
---
+++ public/website_template/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Website Template</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+</head>
+<body>
+	<?php require 'navbar.php'; ?>
+	<?php require 'header.php'; ?>
+	<?php require 'footer.php'; ?>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+</body>
+</html>

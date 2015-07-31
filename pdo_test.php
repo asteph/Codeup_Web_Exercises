cfc3e59c550dd10ff73921d0a1264e64cc1a3327
asteph <alissa.stephens1@gmail.com>
2015-07-31 15:03:40 -0500

    using a pdo to connect to MySQLin the employee database; used constants so that the host, dbname, username, and password can be changed

---
+++ pdo_test.php
@@ -0,0 +1,7 @@
+<?php 
+require 'db_connect.php';
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'employees');
+define('DB_USER', 'codeup');
+define('DB_PASS', 'rocks');
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";

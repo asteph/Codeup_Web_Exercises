3a9a9f3567aa4dabdf57cf3c52ab493370f1a61f
asteph <alissa.stephens1@gmail.com>
2015-07-31 15:06:13 -0500

    changed require to come after the constants, doesn't work otherwise since it would be calling constants before they were defined

--- pdo_test.php
+++ pdo_test.php
@@ -1,7 +1,7 @@
 <?php 
-require 'db_connect.php';
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'employees');
 define('DB_USER', 'codeup');
 define('DB_PASS', 'rocks');
+require 'db_connect.php';
 echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";

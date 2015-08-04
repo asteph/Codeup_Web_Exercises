f94d138823c524c79b7691227ceb65b8edb587c4
asteph <alissa.stephens1@gmail.com>
2015-08-04 11:59:58 -0500

    query loads only four parks at a time and only shows the next and/or previous page links when appropriate

--- public/national_parks.php
+++ public/national_parks.php
@@ -5,8 +5,14 @@ define('DB_USER', 'parks_user');
 define('DB_PASS', '');
 
 require_once '../db_connect.php';
-
-$parks = $dbc->query('SELECT * FROM national_parks')->fetchAll(PDO::FETCH_ASSOC);
+require_once '../Input.php';
+$limit = 4;
+$lastPage = ceil(($dbc->query('SELECT count(*) FROM national_parks')->fetchColumn())/$limit);
+$page = Input::has('page')? Input::get('page') : 1;
+$nextPage = $page + 1; 
+$previousPage = $page - 1; 
+$offset = ($page - 1) * 4; //page number determines how much offset there is
+$parks = $dbc->query("SELECT * FROM national_parks LIMIT $limit OFFSET $offset")->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 <!DOCTYPE html>
@@ -51,5 +57,11 @@ $parks = $dbc->query('SELECT * FROM national_parks')->fetchAll(PDO::FETCH_ASSOC)
 			<? endforeach; ?>
 		</tbody>
 	</table>
+	<?php if($page != 1): ?>
+		<a href="?page=<?= $previousPage ?>" class="btn btn-default">PREVIOUS</a>
+	<?php endif; ?>
+	<?php if($page < $lastPage): ?>
+	<a href="?page=<?= $nextPage ?>" class="btn btn-default">NEXT</a>
+	<?php endif; ?>
 </body>
 </html>

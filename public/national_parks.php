035a123cefe6897c432dd9134e4b48ff9e341595
asteph <alissa.stephens1@gmail.com>
2015-08-04 14:13:08 -0500

    added form that allows national parks to be added to the database using prepared statements for all queries

--- public/national_parks.php
+++ public/national_parks.php
@@ -12,7 +12,21 @@ $page = Input::has('page')? Input::get('page') : 1;
 $nextPage = $page + 1; 
 $previousPage = $page - 1; 
 $offset = ($page - 1) * 4; //page number determines how much offset there is
-$parks = $dbc->query("SELECT * FROM national_parks LIMIT $limit OFFSET $offset")->fetchAll(PDO::FETCH_ASSOC);
+$parks = $dbc->prepare("SELECT * FROM national_parks LIMIT :limit OFFSET :offset");
+$parks->bindValue(':limit', $limit, PDO::PARAM_INT);
+$parks->bindValue(':offset', $offset, PDO::PARAM_INT);
+$parks->execute();
+$parkInfo = $parks->fetchAll(PDO::FETCH_ASSOC);
+
+$newPark = $dbc->prepare('INSERT INTO national_parks (name, location, date_established, area_in_acres, description) VALUES (:name, :location, :date_established, :area_in_acres, :description)');
+if (Input::has('name') && Input::has('location') && Input::has('date_established') && Input::has('area_in_acres') && Input::has('description')){
+	$newPark->bindValue(':name', Input::get('name'), PDO::PARAM_STR);
+    $newPark->bindValue(':location',  Input::get('location'),  PDO::PARAM_STR);
+    $newPark->bindValue(':date_established',  Input::get('date_established'),  PDO::PARAM_STR);
+    $newPark->bindValue(':area_in_acres',  Input::get('area_in_acres'),  PDO::PARAM_INT);
+    $newPark->bindValue(':description',  Input::get('description'),  PDO::PARAM_STR);
+    $newPark->execute();
+}
 
 ?>
 <!DOCTYPE html>
@@ -29,7 +43,11 @@ $parks = $dbc->query("SELECT * FROM national_parks LIMIT $limit OFFSET $offset")
 			width: 80%;
 			margin-left: auto;
 			margin-right: auto;
+			margin-bottom: 4em;
 			padding-top: 4em;
+		}
+		form{
+			margin-top: 4em;
 		}
 	</style>
 	</head>
@@ -46,7 +64,7 @@ $parks = $dbc->query("SELECT * FROM national_parks LIMIT $limit OFFSET $offset")
 		    </tr>
 		</thead>
 		<tbody>
-			<? foreach ($parks as $park): ?>
+			<? foreach ($parkInfo as $park): ?>
 				<tr>
 				    <td><?= $park['name']; ?></td>
 				    <td><?= $park['location']; ?></td>
@@ -63,5 +81,29 @@ $parks = $dbc->query("SELECT * FROM national_parks LIMIT $limit OFFSET $offset")
 	<?php if($page < $lastPage): ?>
 	<a href="?page=<?= $nextPage ?>" class="btn btn-default">NEXT</a>
 	<?php endif; ?>
+
+	<form role="form">
+	  <div class="form-group">
+	    <label for="name">Park Name:</label>
+	    <input name="name" type="text" class="form-control" id="name">
+	  </div>
+	  <div class="form-group">
+	    <label for="location">Location:</label>
+	    <input name="location" type="text" class="form-control" id="location">
+	  </div>
+	  <div class="form-group">
+	    <label for="date_established">Date Established:</label>
+	    <input name="date_established" type="text" class="form-control" id="date-est">
+	  </div>
+	  <div class="form-group">
+	    <label for="Area">Area in Acres:</label>
+	    <input name="area_in_acres" type="text" class="form-control" id="Area">
+	  </div>
+	  <div class="form-group">
+	    <label for="description">Description:</label>
+	    <input name="description" type="textarea" class="form-control" id="description">
+	  </div>
+	  <button type="submit" class="btn btn-default">Submit</button>
+	</form>
 </body>
 </html>
